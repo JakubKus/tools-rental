e4ed5b2242a5cd6d5882e4cc5f7d61b11d80966b
Jakub Kuś <jk052929@student.ath.edu.pl>
2019-05-12 18:33:22 +0200

    connected with db, displayed clients in comboBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ClientsLabel = new System.Windows.Forms.Label();
+            this.ClientsList = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // ClientsLabel
+            // 
+            this.ClientsLabel.AutoSize = true;
+            this.ClientsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.ClientsLabel.Location = new System.Drawing.Point(65, 55);
+            this.ClientsLabel.Name = "ClientsLabel";
+            this.ClientsLabel.Size = new System.Drawing.Size(64, 20);
+            this.ClientsLabel.TabIndex = 0;
+            this.ClientsLabel.Text = "Klienci:";
+            // 
+            // ClientsList
+            // 
+            this.ClientsList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ClientsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ClientsList.FormattingEnabled = true;
+            this.ClientsList.Location = new System.Drawing.Point(69, 78);
+            this.ClientsList.Name = "ClientsList";
+            this.ClientsList.Size = new System.Drawing.Size(162, 24);
+            this.ClientsList.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClientsList);
+            this.Controls.Add(this.ClientsLabel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ClientsLabel;
+        private System.Windows.Forms.ComboBox ClientsList;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,33 @@ namespace toolsRental
         {
             InitializeComponent();
         }
+
+        public SqlCommand ReadFromDb(string query)
+        {
+            String connString = @"Data Source=DESKTOP-2370T16\SQLEXPRESS;Initial Catalog=WypozyczalniaElektronarzedzi;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            return cmd;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            String query = "SELECT Imie, Nazwisko FROM Klienci";
+            var users = ReadFromDb(query);
+            SqlDataReader dataReader = users.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                ClientsList.Items.Add(String.Format("{0} {1}",
+                    dataReader.GetValue(0),
+                    dataReader.GetValue(1)));
+            }
+
+            dataReader.Close();
+            users.Dispose();
+            users.Connection.Close();
+        }
     }
 }
