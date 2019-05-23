0499d68a7e7124df7f3de2fc97564a6c35e03c3d
Jakub Kuś <jk052929@student.ath.edu.pl>
2019-05-23 22:12:38 +0200

    added AddClientForm with fields validation

---
+++ AddClientForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace toolsRental
+{
+    partial class AddClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.FirstNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.AddClientButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(15, 10, 0, 0);
+            this.label2.Size = new System.Drawing.Size(60, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Imię:";
+            // 
+            // FirstNameBox
+            // 
+            this.FirstNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FirstNameBox.Location = new System.Drawing.Point(19, 33);
+            this.FirstNameBox.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.FirstNameBox.Name = "FirstNameBox";
+            this.FirstNameBox.Size = new System.Drawing.Size(152, 23);
+            this.FirstNameBox.TabIndex = 7;
+            this.FirstNameBox.TextChanged += new System.EventHandler(this.FirstNameBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            this.label1.Location = new System.Drawing.Point(0, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(15, 10, 0, 0);
+            this.label1.Size = new System.Drawing.Size(101, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nazwisko:";
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.LastNameBox.Location = new System.Drawing.Point(19, 89);
+            this.LastNameBox.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(152, 23);
+            this.LastNameBox.TabIndex = 9;
+            this.LastNameBox.TextChanged += new System.EventHandler(this.LastNameBox_TextChanged);
+            // 
+            // AddClientButton
+            // 
+            this.AddClientButton.Enabled = false;
+            this.AddClientButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.AddClientButton.Location = new System.Drawing.Point(19, 132);
+            this.AddClientButton.Name = "AddClientButton";
+            this.AddClientButton.Size = new System.Drawing.Size(152, 38);
+            this.AddClientButton.TabIndex = 10;
+            this.AddClientButton.Text = "Dodaj użytkownika";
+            this.AddClientButton.UseVisualStyleBackColor = true;
+            this.AddClientButton.Click += new System.EventHandler(this.AddClientButton_Click);
+            // 
+            // AddClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddClientButton);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FirstNameBox);
+            this.Controls.Add(this.label2);
+            this.Name = "AddClientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FirstNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.Button AddClientButton;
+    }
+}
---
+++ AddClientForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace toolsRental
+{
+    public partial class AddClientForm : Form
+    {
+        public AddClientForm()
+        {
+            InitializeComponent();
+        }
+        Queries queries = new Queries();
+        private bool CheckNameBox()
+        {
+            return FirstNameBox.Text.Length > 2
+                && LastNameBox.Text.Length > 2
+                && Regex.IsMatch(FirstNameBox.Text, @"^[a-zA-Z]+$")
+                && Regex.IsMatch(LastNameBox.Text, @"^[a-zA-Z]+$");
+
+        }
+        private void FirstNameBox_TextChanged(object sender, EventArgs e)
+        {
+            AddClientButton.Enabled = CheckNameBox();
+        }
+        private void LastNameBox_TextChanged(object sender, EventArgs e)
+        {
+            AddClientButton.Enabled = CheckNameBox();
+        }
+        private void AddClientButton_Click(object sender, EventArgs e)
+        {
+            string addClientQuery = "INSERT INTO Klienci (Imie, Nazwisko) VALUES" +
+            " ('" + FirstNameBox.Text + "', '" + LastNameBox.Text + "')";
+            var addClient = queries.ExecuteQuery(addClientQuery);
+            addClient.ExecuteNonQuery();
+            Close();
+        }
+    }
+}
